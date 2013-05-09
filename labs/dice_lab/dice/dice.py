32be56b131e81ff660edeed65cc41378be07def3
Jason McVetta <jason.mcvetta@gmail.com>
2013-05-09 11:54:56 -0700

    split dice library from roll script

---
+++ labs/dice_lab/dice/dice.py
@@ -0,0 +1,12 @@
+# This is Free Software, released under the terms of the X11 License.
+# See http://directory.fsf.org/wiki/License:X11 for details.
+
+from die import Die
+
+class Die6(Die):
+    '''A six-sided die'''
+    pass
+
+class Die20(Die):
+    '''A twenty-sided die'''
+    pass
